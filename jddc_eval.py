c23b0e8d7aa6549bc37c8b2ed315522f65dc4c30
MozarTuring <Jingwei_M@163.com>
2018-09-21 15:13:56 +0800

    Create jddc_eval.py

---
+++ jddc_eval.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.append('single/nmt/')
+
+# ======== Replace with your real import ======== #
+from run_prediction import run_prediction
+
+if __name__ == '__main__':
+    try:
+        input_file_path = sys.argv[1]
+        output_file_path = sys.argv[2]
+    except IndexError:
+        print('Two positional arguments are required: input_file_path, output_file_path')
+        exit(1)
+
+    if not os.path.isfile(input_file_path):
+        print('Input file is not found: %s' % input_file_path)
+        exit(1)
+
+#    output_dir = os.path.dirname(output_file_path)
+#    if not os.path.isdir(output_dir):
+#        print('Output dir is not exist: %s' % output_dir)
+#        exit(1)
+
+    # ======== Replace with your real code ======== #
+#    model_dir = 'single/jb_attention'
+#    data_dir = 'data/'
+    multi_ind = 50
+    run_prediction(input_file_path, output_file_path, multi_ind)
+
